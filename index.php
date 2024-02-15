e7f5bd1045244d59faf8e88cea5e3a6bba59f873
Ndipaulin <ndipaulin@gmail.com>
2024-02-15 18:01:21 +0200

    this is modified Devops page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SCHOOL</title>
+</head>
+<body>
+<?php>
+echo "<h1>welcome to this page</h1>" 
+</body>
+</html>
